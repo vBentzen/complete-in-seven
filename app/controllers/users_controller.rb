1b357fdc94d4153a18537c0849f390577ad0bc05
Aske Bentzen <askebentzen@gmail.com>
2017-12-04 22:45:25 +0100

    changed show to look for current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
   resources :users, only: [:show]
   get 'about' => 'welcome#about'
 
