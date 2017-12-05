8933a6f5daa03c9adac2dca486d26ae843d7b12a
Aske Bentzen <askebentzen@gmail.com>
2017-12-05 21:00:21 +0100

    can create and view to do items @user.profile

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,20 @@
 class ItemsController < ApplicationController
-  def new
-    @item = Item.new
-  end
+
 
   def create
-    @item = Item.new
-    @item.name = params[:item][:name]
+    @user = current_user
+    item = @user.items.new(item_params)
 
-    if @item.save
+    if item.save
       flash[:notice] = "Item was added to your list."
     else
       flash[:alert] = "There was an error saving the item, please try again."
     end
 
     redirect_to root_path
+  end
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  let(:user) { create(:user) }
-  let(:item) { create(:item) }
+  RSpec.describe Item, type: :model do
+
+    let(:user) { create(:user) }
+    let(:item) { create(:item) }
 
 
-  it { is_expected.to belong_to(:user) }
-end
-
+    it { is_expected.to belong_to(:user) }
+  end
